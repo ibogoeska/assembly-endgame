0bf094d81edf45e0db4d1ba3891aa2c4b77b4a1d
ibogoeska <i.bogoeskaa@gmail.com>
2026-01-28 13:30:26 +0100

    Function for time remaining and remaining attempts to try and guess the word added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { languages } from "../languages";
 import Hangman from "./components/Hangman";
 import LanguageChip from "./components/LanguageChip";
 import Status from "./components/Status";
 import clsx from "clsx";
+import { getFarewellText } from "../utils";
+import { words } from "../words";
+import Confetti from "react-confetti";
 
 const App = () => {
   //Static values
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const numGuessesLeft = languages.length - 1;
+  const randomWord = words[Math.floor(Math.random() * words.length)];
+  const timeRef = useRef(null);
 
   // State values
-  const [currentWord, setCurrentWord] = useState("react");
+  const [currentWord, setCurrentWord] = useState(randomWord);
   const [guessedLetters, setGuessedLetters] = useState([]);
+  const [showFarawell, setShowFarawell] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(60);
+  const [isTimerActive, setIsTimerActive] = useState(false);
+  const [attempts, setAttempts] = useState(languages.length - 1);
 
   //Derived values
+  const isTimeUp = timeRemaining === 0;
   const wrongGuestCount = guessedLetters.filter((l) => {
     return !currentWord.includes(l);
   }).length;
+
   const isGameWon = currentWord
     .split("")
     .every((l) => guessedLetters.includes(l));
-  const isGameLost = wrongGuestCount === languages.length - 1;
+  const isGameLost = wrongGuestCount === languages.length - 1 || isTimeUp;
   const isGameOver = isGameWon || isGameLost;
 
+  // time counter on component mount after clicking the start button
+  useEffect(() => {
+    if (!isTimerActive) return;
+    timeRef.current = setInterval(() => {
+      setTimeRemaining((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timeRef.current);
+  }, [isTimerActive]);
+
+  useEffect(() => {
+    if (timeRemaining <= 0 || isGameOver) {
+      clearInterval(timeRef.current);
+      setIsTimerActive(false);
+    }
+  }, [timeRemaining, isGameOver]);
+
+  const startGame = () => {
+    setTimeRemaining(60);
+    setIsTimerActive(true);
+  };
+
   const addGuessedLetter = (letter) => {
+    if (!guessedLetters.includes(letter)) {
+      if (!currentWord.includes(letter)) {
+        const newWrongGuess = wrongGuestCount + 1;
+        const farawellMessage = getFarewellText(
+          languages[newWrongGuess - 1].name,
+        );
+        setShowFarawell(farawellMessage);
+        setAttempts((prev) => prev - 1);
+      }
+    }
     setGuessedLetters(
       (prev) => (prev.includes(letter) ? prev : [...prev, letter]),
 
@@ -33,11 +77,19 @@ const App = () => {
     );
   };
 
-  const letterElements = currentWord.split("").map((el, index) => (
-    <span key={index} className="letter">
-      {guessedLetters.includes(el) ? el.toUpperCase() : ""}
-    </span>
-  ));
+  const letterElements = currentWord.split("").map((el, index) => {
+    const shouldRevealLetter = isGameLost || guessedLetters.includes(el);
+    const letterClassName = clsx(
+      isGameLost && !guessedLetters.includes(el) && "missed-letter",
+      !isGameLost && guessedLetters.includes(el) && "letter",
+    );
+
+    return (
+      <span key={index} className={letterClassName}>
+        {shouldRevealLetter ? el.toUpperCase() : ""}
+      </span>
+    );
+  });
 
   const languageChips = languages.map((l, index) => (
     <LanguageChip
@@ -64,24 +116,66 @@ const App = () => {
         key={letter}
         value={letter}
         onClick={() => addGuessedLetter(letter)}
+        disabled={isGameOver}
+        aria-disabled={guessedLetters.includes(letter)}
+        aria-label={`Letter ${letter}`}
       >
         {letter.toUpperCase()}
       </button>
     );
   });
 
+  const startNewGame = () => {
+    setCurrentWord(randomWord);
+    setGuessedLetters([]);
+    setShowFarawell("");
+    setIsTimerActive(false);
+    setTimeRemaining(60);
+    setAttempts(languages.length - 1);
+  };
   return (
     <>
+      {isGameWon && <Confetti recycle={false} numberOfPieces={1000} />}
       <Hangman />
+      <div className="timer">Time remaining: {timeRemaining}s</div>
+      <div className="remaining-attempts">
+        You have <span>{attempts} </span> attempts more to try and guess the
+        word
+      </div>
       <Status
         isGameOver={isGameOver}
         isGameLost={isGameLost}
         isGameWon={isGameWon}
+        isTimeUp={isTimeUp}
+        showFarawell={showFarawell}
       />
       <section className="language-chips">{languageChips}</section>
       <section className="quessing-word">{letterElements}</section>
+      <section className="sr-only" aria-live="polite" role="guessing-word">
+        <p>
+          Current word:
+          {currentWord
+            .split("")
+            .map((letter) =>
+              guessedLetters.includes(letter) ? letter + "." : "blank",
+            )
+            .join(" ")}
+        </p>
+        <p>You have {numGuessesLeft} attempts left.</p>
+      </section>
       <section className="keyboard">{keybordLetters}</section>
-      {isGameOver && <button className="new-game">New Game</button>}
+
+      {isGameOver ? (
+        <button className="new-game" onClick={startNewGame}>
+          New Game
+        </button>
+      ) : !isTimerActive ? (
+        <button className="start-game" onClick={startGame}>
+          Start
+        </button>
+      ) : (
+        ""
+      )}
     </>
   );
 };
--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,17 +1,39 @@
-const Status = ({ isGameOver, isGameLost, isGameWon }) => {
+const Status = ({
+  isGameOver,
+  isGameLost,
+  isGameWon,
+  isTimeUp,
+  showFarawell,
+}) => {
   return (
-    <section className="game-status">
-      {isGameOver && isGameWon && (
+    <section className="game-status" aria-live="polite" role="status">
+      {/* WIN */}
+      {isGameWon && (
         <div className="game-won">
-          <h2>You win! </h2>
+          <h2>You win!</h2>
           <p>Well done!</p>
         </div>
       )}
-      {isGameOver && isGameLost && (
+
+      {/* LOSS BY TIME */}
+      {!isGameWon && isGameLost && isTimeUp && (
+        <div className="game-lost">
+          <h2>Time’s up!</h2>
+          <p>You ran out of time.</p>
+        </div>
+      )}
+
+      {/* LOSS BY WRONG GUESSES */}
+      {!isGameWon && isGameLost && !isTimeUp && (
         <div className="game-lost">
           <h2>Game over!</h2>
-          <p>You might better start learning Assembly!</p>
+          <p>Better start learning Assembly!</p>
         </div>
+      )}
+
+      {/* MID-GAME FAREWELL */}
+      {!isGameOver && showFarawell && (
+        <p className="farawell-msg">"{showFarawell}"</p>
       )}
     </section>
   );
